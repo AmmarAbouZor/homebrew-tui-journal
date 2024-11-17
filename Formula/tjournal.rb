25f70c0a2e10e70305f9e29142a5115b37c345e4
Ammar Abou Zor <35040531+AmmarAbouZor@users.noreply.github.com>
2024-11-17 11:12:30 +0000

    tjournal 0.12.1

--- Formula/tjournal.rb
+++ Formula/tjournal.rb
@@ -15,8 +15,8 @@ class Tjournal < Formula
 
     on_linux do
       on_intel do
-        url "https://github.com/AmmarAbouZor/tui-journal/archive/v0.12.0/tjournal-linux-gnu.tar.gz"
-        sha256 "fb752861a39b66b44d9cb977cb840012a5e307ffde788b6f458d579bcf5b42d5"
+        url "https://github.com/AmmarAbouZor/tui-journal/archive/v0.12.1/tjournal-linux-gnu.tar.gz"
+        sha256 "e06a1d1314e0f80ee33f57bd5e830ff5e29fe546d34891ee195588ff4514b0bd"
       end
     end
     
