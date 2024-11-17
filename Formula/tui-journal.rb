ceeeb8d149288296d0f9cfada334ba657abadfab
Ammar Abou Zor <ammarabouzor@gmail.com>
2024-11-17 16:58:31 +0100

    Use `cargo install` in formula

--- Formula/tui-journal.rb
+++ Formula/tui-journal.rb
@@ -1,26 +1,13 @@
 class TuiJournal < Formula
-    desc "Your journal app if you live in a terminal"
-    homepage "https://crates.io/crates/tui-journal"
-  
-    on_macos do
-      on_arm do
-        url "https://github.com/AmmarAbouZor/tui-journal/releases/download/v0.12.1/tjournal-mac-arm64.tar.gz"
-        sha256 "bd518d851103d12db23dfbc5a9d4338a097f2bcf7de8e0c79cb8b948b456f8b0"
-      end
-      on_intel do
-        url "https://github.com/AmmarAbouZor/tui-journal/releases/download/v0.12.1/tjournal-mac-x86_64.tar.gz"
-        sha256 "3054291204bf44113038d60fb3210d45844444ee68b3c1f63fd04ef217a88cd6"
-      end
-    end
+  desc "Your journal app if you live in a terminal"
+  homepage "https://github.com/AmmarAbouZor/tui-journal"
+  url "https://github.com/AmmarAbouZor/tui-journal/archive/refs/tags/v0.12.0.tar.gz"
+  sha256 "646b83fbd638cfabb7c23d3a76cf68ffa97dae26571d40a40637e77d6bdc2984"
+  license "MIT"
 
-    on_linux do
-      on_intel do
-        url "https://github.com/AmmarAbouZor/tui-journal/releases/download/v0.12.1/tjournal-linux-gnu.tar.gz"
-        sha256 "fb752861a39b66b44d9cb977cb840012a5e307ffde788b6f458d579bcf5b42d5"
-      end
-    end
-    
-    def install
-      bin.install "tjournal"
-    end
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
+end
