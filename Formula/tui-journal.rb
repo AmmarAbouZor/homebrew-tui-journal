ee8b483e531d8610bd39c30546437a2748de99ba
Ammar Abou Zor <35040531+AmmarAbouZor@users.noreply.github.com>
2024-11-25 20:22:54 +0100

    tui-journal 0.13.0 (#8)

--- Formula/tui-journal.rb
+++ Formula/tui-journal.rb
@@ -1,8 +1,8 @@
 class TuiJournal < Formula
   desc "Your journal app if you live in a terminal"
   homepage "https://github.com/AmmarAbouZor/tui-journal"
-  url "https://github.com/AmmarAbouZor/tui-journal/archive/refs/tags/v0.12.1.tar.gz"
-  sha256 "e06a1d1314e0f80ee33f57bd5e830ff5e29fe546d34891ee195588ff4514b0bd"
+  url "https://github.com/AmmarAbouZor/tui-journal/archive/refs/tags/v0.13.0.tar.gz"
+  sha256 "7f5e2b0b1b1a227cf9154c1f4749a0a645fc18a6cfa9750872b0b45400b1a0a6"
   license "MIT"
 
   depends_on "rust" => :build
